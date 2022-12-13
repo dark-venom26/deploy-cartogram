a9b238b141bc401135d5fdcd6aa0ff95d7f0554a
Aman Vishwakarma <92418471+dark-venom26@users.noreply.github.com>
2022-12-13 10:52:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -36,11 +36,11 @@ app.use("/api/v1", userRoute)
 app.use("/api/v1", orderRoute)
 app.use("/api/v1", paymentRoute)
 
-// app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.use(express.static(path.join(__dirname,"../frontend/build")));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+})
 
 // Middleware for errors
 app.use(errorMiddleware);
